8d87e0e924b2e25e82393ef0b34392fd70d02116
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-08-18 16:10:53 +0100

    Updated the slugify function

--- helpers/text.py
+++ helpers/text.py
@@ -5,4 +5,10 @@ def slugify(text:str) -> str:
     for replace_me in to_replace:
         if replace_me in text:
             text = text.replace(replace_me, slug_char)
+    while slug_char*2 in text:
+        text = text.replace(slug_char*2, slug_char)
+    if text[0] == slug_char:
+        text = text[1:]
+    if text[-1] == slug_char:
+        text = text[:-1]
     return text.lower()
